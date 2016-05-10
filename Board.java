5dc27e718083abe0ed60e0877ec50b08c5080217
CromwellCutaran <CromwellCutaran@gmail.com>
2016-05-10 10:17:05 -0700

    Implemented custom Comparator<Board> for PriorityQueue

--- Board.java
+++ Board.java
@@ -2,8 +2,13 @@ import java.util.*;
 
 public class Board {
 
+    int[][] previous;
     int[][] board;
+    int hPriority;
 
+    /**
+     * initial Board constructor
+     */
     public Board() {
         board = new int[3][3];
         for (int r = 0; r < 3; r++) {
@@ -13,6 +18,12 @@ public class Board {
         }
     }
 
+    /**
+     * Currently tester class for new Board states
+     * @param b
+     */
+    public Board(int[][] b) { board = b; }
+
     public void showBoard() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -20,5 +31,26 @@ public class Board {
             }
             System.out.println(" ");
         }
+    }
+
+    /**
+     * Hamming priority function defines the priority of a state
+     * as the number of blocks in the wrong position, plus the number
+     * of moves made so far TODO: add number of moves made
+     * @return Hamming priority
+     */
+    public int getHammingPriority() {
+
+        int[][] goal = {{ 1, 2, 3, },
+                        { 4, 5, 6, },
+                        { 7, 8, 0} };
+        hPriority = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] != goal[i][j])
+                    hPriority++;
+            }
+        }
+        return hPriority;
     }
 }
--- Solver.java
+++ Solver.java
@@ -1,21 +1,85 @@
+/**
+ * TODO: How to implement storing previous state and adjacency list for current state
+ */
+
 import java.util.*;
 
 
 public class Solver {
+
+    private int moves;
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the initial board, one row at a time.\n" +
                            "Use '0' to represent the empty tile.");
 
-        Board board = new Board();
+        Board initial = new Board();
 
-        board.showBoard();
+        initial.showBoard();
 
-        Solver solution = new Solver(board);
+        Solver solution = new Solver(initial);
+
     }
 
-    public Solver(Board board) {
+    /**
+     * Constructor for a board's solution. Finds the actual solution
+     * of a board, adding to an ArrayList<Board> containing every
+     * state of the board seen along the way.
+     * @param initial Board to be solved
+     */
+    public Solver(Board initial) {
 
+        PriorityQueue<Board> boardQueue = new PriorityQueue<>(new BoardComparator());
+
+    }
+
+    /**
+     * @return # of moves taken to reach solution
+     */
+    public int moves() {
+        return moves;
+    }
+
+    /**
+     *  Since we use a PriorityQueue to decide which board state to dequeue next,
+     *  we must use a custom Comparator with which we can instantiate our
+     *  PriorityQueue. BoardComparator uses the Hamming priority function
+     *  implemented in Board.java to return a state's priority.
+     */
+    static class BoardComparator implements Comparator<Board> {
+
+        public int compare(Board b1, Board b2) {
+            return b1.getHammingPriority() - b2.getHammingPriority();
+        }
+    }
+
+    /**
+     * Test to show that BoardComparator is indeed working properly
+     */
+    public static void testPriorityQueue() {
+        BoardComparator bc = new BoardComparator();
+
+        PriorityQueue<Board> boardQueue = new PriorityQueue<>(bc);
+
+        int[][] b1 = {{ 1, 2, 3, },
+                      { 4, 0, 5, },
+                      { 6, 7, 8} };
+
+        int[][] b2 = {{ 1, 2, 3, },
+                      { 4, 5, 0, },
+                      { 6, 7, 8} };
+
+        Board board1 = new Board(b1);
+        Board board2 = new Board(b2);
+
+        boardQueue.add(board1);
+        boardQueue.add(board2);
+
+        System.out.println(board1.getHammingPriority() + "\n" + board2.getHammingPriority());
+
+        Board b = boardQueue.poll(); // should give b2 to b
+
+        b.showBoard();
     }
 }
